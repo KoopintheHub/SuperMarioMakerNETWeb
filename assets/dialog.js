6b4f1bb1f8314cb10a0c50b431b60e683c175331
Koop <61279093+KoopintheHub@users.noreply.github.com>
2020-10-28 08:02:28 +0000

    Update dialog.js

--- assets/dialog.js
+++ assets/dialog.js
@@ -8,9 +8,12 @@ function displayDialog(innerHTML) {
         darken = true;
         document.getElementById('darken').style = "pointer-events: all; background: rgba(0,0,0,0.5); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15";
         document.getElementById('darken').innerHTML = "<div style=\"opacity: 1; background-color: rgba(255, 255, 255, 0.5); border: 1px solid black; display: block; width: 50%; height: 50%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)\">" + innerHTML + "</div>";
+        $('#darken').fadeOut(0);
+        $('#darken').fadeIn(150);
     } else {
         darken = false;
-        document.getElementById('darken').style = "pointer-events: none; background: rgba(0,0,0,0); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15";
-        document.getElementById('darken').innerHTML = "";
+        // document.getElementById('darken').style = "pointer-events: none; background: rgba(0,0,0,0); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15";
+        // document.getElementById('darken').innerHTML = "";
+        $('#darken').fadeOut(150);
     }
 }
