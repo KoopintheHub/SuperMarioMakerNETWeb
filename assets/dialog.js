0a91cb7e92a64e001e7613b0e3e2ccf758b3bfc3
Koop <61279093+KoopintheHub@users.noreply.github.com>
2020-10-28 08:31:03 +0000

    Update dialog.js

--- assets/dialog.js
+++ assets/dialog.js
@@ -6,8 +6,8 @@ var darken = false;
 function displayDialog(width, height, innerHTML) {
     if (!darken) {
         darken = true;
-        document.getElementById('darken').style = "pointer-events: all; background: rgba(0,0,0,0.5); border-radius: 18px; width: " + width + "%; height: " + height + "%; position: fixed; top: 0px; left: 0px; z-index: 15";
-        document.getElementById('darken').innerHTML = "<div style=\"opacity: 1; background-color: rgba(255, 255, 255, 0.5); border: 1px solid black; display: block; width: 50%; height: 50%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)\">" + innerHTML + "</div>";
+        document.getElementById('darken').style = "pointer-events: all; background: rgba(0,0,0,0.5); width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: 15";
+        document.getElementById('darken').innerHTML = "<div style=\"opacity: 1; background-color: rgba(255, 255, 255, 0.5); border: 1px solid black; border-radius: 18px; display: block; width: " + width + "%; height: " + height + "%; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)\">" + innerHTML + "</div>";
         $('#darken').fadeOut(0);
         $('#darken').fadeIn(150);
     } else {
